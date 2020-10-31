0e5231df17b5f86ac040d2be20b454fcaf7d9fde
Rory <rory.magee2@gmail.com>
2020-10-31 18:09:47 +0000

    sidebar will be removed when twitter is loaded

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
-console.log('plugin working');
+findTrending();
+function findTrending() {
+    let trending = document.querySelector("div[data-testid='sidebarColumn']");
+    if (trending) {
+        trending.remove();
+    }
+    setTimeout(() => {
+        findTrending();
+    }, 1000);
+}
